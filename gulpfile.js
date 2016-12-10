23600511374b25b26a74d7d0f8935acc612bd2a0
Daniel Johansson <daniel.johansson@ehr-lap-001.local>
2016-12-10 14:13:14 +0100

    Added Compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -10,6 +11,15 @@ var jsSources = [
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
+];
+var sassSources = [
+	'components/sass/base.scss',
+	'components/sass/mixins.scss',
+	'components/sass/modules.scss',
+	'components/sass/pixgrid.scss',
+	'components/sass/rclick.scss',
+	'components/sass/variables.scss',
+	'components/sass/style.scss'
 ];
 
 gulp.task('coffee', function() {
@@ -25,3 +35,15 @@ gulp.task('js', function() {
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
+});
+
